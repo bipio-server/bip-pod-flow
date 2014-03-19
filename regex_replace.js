7c9be9c10bbc21760f3b27f65a7e1525ddad2b2f
michael pearson <gravatar@x0rz.com>
2014-03-18 20:10:22 -0400

    added safe regex check

--- regex_replace.js
+++ regex_replace.js
@@ -16,6 +16,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+var safeRegex = require('safe-regex');
+
 function RegExpReplace(podConfig) {
   this.name = 'regex_replace';
   this.description = 'Regex Replace',
@@ -58,14 +61,18 @@ RegExpReplace.prototype.getSchema = function() {
 
 RegExpReplace.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
   if (imports.in_str && imports.repl_str && imports.regex) {
-    try {
-      var exports = { 
-          out_str : imports.in_str.replace(new RegExp(imports.regex, 'gi'), imports.repl_str) 
-        };
+    if (safeRegex(imports.regex)) {
+      try {
+        var exports = { 
+            out_str : imports.in_str.replace(new RegExp(imports.regex, 'gi'), imports.repl_str) 
+          };
 
-      next(false, exports);
-    } catch (e) {
-      next(e.message);
+        next(false, exports);
+      } catch (e) {
+        next(e.message);
+      }
+    } else {
+      next('Regex ' + imports.regex + ' is unsafe');
     }
   } else {
     // silent passthrough
